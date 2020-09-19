4326d1b4196028afb91d0d6be5a1551102998040
freesummerwind <liz-e-tta@yandex.ru>
2020-09-19 22:30:54 +0300

    Add dichotomy function

--- main.cpp
+++ main.cpp
@@ -21,6 +21,41 @@ const double LOWER_EDGE = -2.;
 const double UPPER_EDGE = 0.;
 const double EPSILON = .1;
 
+void dichotomy(double lower, double upper) {
+    cout << "Part 1. Finding minimum of the function with dichotomy method\n\n"
+         << std::string(76, '_') << '\n'
+         << '|' << std::string(3, ' ') << "Start of" << std::string(3, ' ')
+         << '|' << std::string(4, ' ') << "End of" << std::string(4, ' ')
+         << '|' << std::string(2, ' ') << "Length of" << std::string(3, ' ')
+         << '|' << std::string(14, ' ')
+         << '|' << std::string(14, ' ') << '|' << '\n'
+         << '|' << std::string(1, ' ') << "the interval" << std::string(1, ' ')
+         << '|' << std::string(1, ' ') << "the interval" << std::string(1, ' ')
+         << '|' << std::string(1, ' ') << "the interval" << std::string(1, ' ')
+         << '|' << std::string(4, ' ') << "f(ak)" << std::string(5, ' ')
+         << '|' << std::string(4, ' ') << "f(bk)" << std::string(5, ' ') << '|' << '\n'
+         << '|' << std::string(5, ' ') << "(ak)" << std::string(5, ' ')
+         << '|' << std::string(5, ' ') << "(bk)" << std::string(5, ' ')
+         << '|' << std::string(5, ' ') << "(l)" << std::string(6, ' ')
+         << '|' << std::string(14, ' ')
+         << '|' << std::string(14, ' ') << '|' << '\n'
+         << std::string(76, '_') << '\n';
+    const double DELTA = .01;
+    while(upper - lower > EPSILON) {
+        beautifulPrintingForPart1(lower, upper);
+        double x1 = lower + (upper - lower) / 2 - DELTA,
+                x2 = lower + (upper - lower) / 2 + DELTA;
+        myFunctionFromTask(x1) < myFunctionFromTask(x2)
+        ? upper = x1
+        : lower = x2;
+    }
+    cout << '|' << std::setw(13) << lower << ' '
+         << '|' << std::setw(13) << upper << ' '
+         << '|' << std::setw(13) << upper - lower << ' '
+         << '|' << std::string(9, ' ') << "l < epsilon" << std::string(9, ' ') << '|' << '\n'
+         << std::string(76, '_') << '\n';
+}
+
 int main() {
     cout << "Variant 4.\nFunction: cos(x)*th(x)\nInterval: [" << LOWER_EDGE << " " << UPPER_EDGE << "]\n";
     cout << "Part 1. Finding minimum of the function with dichotomy method\n";
